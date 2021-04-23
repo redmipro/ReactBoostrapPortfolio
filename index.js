9f3e6bd1f08340ec29d5d4713e34fa103c6f0008
redmipro <xiaominredmipro@gmail.com>
2021-04-22 22:18:29 -0400

    Git Branch develop-2 test

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//	<!--prueba gitlab toroise-->
+//	<!--branch develo-2-->
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
